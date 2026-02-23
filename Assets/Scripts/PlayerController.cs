96794647b0753ed18c37de110b60f46354a170f6
Umair K <umair.khan7@education.nsw.gov.au>
2026-02-23 11:55:15 +1100

    Game Finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -37,6 +37,7 @@ public class PlayerController : MonoBehaviour
       if (count >= 12) 
       {
         winTextObject.SetActive(true);
+         Destroy(GameObject.FindGameObjectWithTag("Enemy"));
       }
    }
 
@@ -47,6 +48,17 @@ public class PlayerController : MonoBehaviour
       Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
       rb.AddForce(movement * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
